333031769fc60a090c85817cb8991856560ee486
Com-Zepol <lopez.eric0817@gmail.com>
2023-04-12 10:43:12 -0400

    Add files via upload

---
+++ Program 3/Prog3/RegForm.cs
@@ -0,0 +1,165 @@
+﻿// Program 3
+// CIS 199-01
+// Due: 11/07/2019
+// By: J3408
+// Rewrote sections of code from if statements deciding time and date decision to parrellal arrays using range matching
+
+// This application calculates the earliest registration date
+// and time for an undergraduate student given their class standing
+// and last name.
+// Decisions based on UofL Spring 2020 Priority Registration Schedule
+
+// Solution 3
+// This solution keeps the first letter of the last name as a char
+// and uses if/else logic for the times.
+// It uses defined strings for the dates and times to make it easier
+// to maintain.
+// It only uses programming elements introduced in the text or
+// in class.
+// This solution takes advantage of the fact that there really are
+// only two different time patterns used. One for juniors and seniors
+// and one for sophomores and freshmen. The pattern for sophomores
+// and freshmen is complicated by the fact the certain letter ranges
+// get one date and other letter ranges get another date.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace Prog3
+{
+    public partial class RegForm : Form
+    {
+        public RegForm()
+        {
+            InitializeComponent();
+        }
+
+        // Find and display earliest registration time
+        private void FindRegTimeBtn_Click(object sender, EventArgs e)
+        {
+            const string DAY1 = "November 4"; // 1st day of registration
+            const string DAY2 = "November 5"; // 2nd day of registration
+            const string DAY3 = "November 6"; // 3rd day of registration
+            const string DAY4 = "November 7";  // 4th day of registration
+            const string DAY5 = "November 8";  // 5th day of registration
+            const string DAY6 = "November 11";  // 6th day of registration
+
+            const string TIME1 = "8:30 AM";  // 1st time block
+            const string TIME2 = "10:00 AM"; // 2nd time block
+            const string TIME3 = "11:30 AM"; // 3rd time block
+            const string TIME4 = "2:00 PM";  // 4th time block
+            const string TIME5 = "4:00 PM";  // 5th time block
+
+            const float SOPHOMORE = 30; // Hours needed to be sophomore
+            const float JUNIOR = 60;    // Hours needed to be junior
+            const float SENIOR = 90;    // Hours needed to be senior
+
+            string lastNameStr;         // Entered last name
+            char lastNameLetterCh;      // First letter of last name, as char
+            string dateStr = "Error";   // Holds date of registration
+            string timeStr = "Error";   // Holds time of registration
+            float creditHours;          // Previously earned credit hours
+            bool isUpperClass;          // Upperclass or not?
+
+            lastNameStr = lastNameTxt.Text;
+            if (lastNameStr.Length > 0) // Empty string?
+            {
+                lastNameLetterCh = lastNameStr[0];   // First char of last name
+                lastNameLetterCh = char.ToUpper(lastNameLetterCh); // Ensure upper case         // taking first letter from userinput string
+
+                if (float.TryParse(creditHoursTxt.Text, out creditHours) && creditHours >= 0)
+                {
+                    if (char.IsLetter(lastNameLetterCh)) // Is it a letter?
+                    {
+                        isUpperClass = (creditHours >= JUNIOR);                                 // junior and above is upperclassmen
+
+                        // Juniors and Seniors share same schedule but different days
+                        if (isUpperClass)
+                        {
+                            if (creditHours >= SENIOR)
+                                dateStr = DAY1;
+                            else // Must be juniors
+                                dateStr = DAY2;
+                                            
+                                                /* J3408 */
+                            char[] SeniorJuniorLastNameLetters = { 'A', 'E', 'J', 'P', 'T' };                 /* Char array of lower limit last name letters for seniors and juniors */
+                            string[] FreshSophTimes = { TIME3, TIME4, TIME5, TIME1, TIME2 };                  /* String array of time slots for seniors and juniors */
+
+                            bool found = false;                                                               /* Boolean variable set to false */
+                            int index = SeniorJuniorLastNameLetters.Length - 1;                               /* Starting from the end of the array */
+
+                            while (index >= 0 && !found)
+                            {
+                                if (lastNameLetterCh >= SeniorJuniorLastNameLetters[index])                    /* Sets the found to true if letter is found on array*/
+                                    found = true;
+                                else
+                                    --index;
+                            }
+
+                            if (found)                                                                         /* When found (true) produces the following */
+                            {
+                                timeStr = FreshSophTimes[index];                                               /* When found on the parallel array sets the value to this string to display on GUI */
+                            }
+                        }
+                        else // Must be soph/fresh
+                        {
+                            if (creditHours >= SOPHOMORE)
+                            {
+                                // A-B, P-Z on day one
+                                if ((lastNameLetterCh <= 'B') ||  // <= B
+                                    (lastNameLetterCh >= 'P'))    // >= P
+                                    dateStr = DAY3;
+                                else // All other letters on next day
+                                    dateStr = DAY4;
+                            }
+                            else // must be freshman
+                            {
+                                // A-B, P-Z on day one
+                                if ((lastNameLetterCh <= 'B') ||  // <= B
+                                    (lastNameLetterCh >= 'P'))    // >= P
+                                    dateStr = DAY5;
+                                else // All other letters on next day
+                                    dateStr = DAY6;
+                            }
+                                                /* J3408 */
+                            char[] FreshSophLastNameLetters = { 'A', 'C', 'E', 'G', 'J', 'M', 'P', 'R', 'T', 'W' };                     /* Char array of lower limit last name letters for freshmen and sophomore */
+                            string[] FreshSophTimes = { TIME5, TIME1, TIME2, TIME3, TIME4, TIME5, TIME1, TIME2, TIME3, TIME4 };         /* String array of time slots for freshmen and sophomore */
+
+                            bool found = false;                                                                                         /* Boolean variable set to false */
+                            int index = FreshSophLastNameLetters.Length - 1;                                                            /* Starting from the end of the array */
+
+                            while (index >= 0 && !found)
+                            {
+                                if (lastNameLetterCh >= FreshSophLastNameLetters[index])                                                /* Sets the found to true if letter is found on array*/
+                                    found = true;
+                                else
+                                    --index;
+                            }
+
+                            if (found)                                                                                                  /* When found (true) produces the following */
+                            {
+                                timeStr = FreshSophTimes[index];                                                                        /* When found on the parallel array sets the value to this string to display on GUI */
+                            }
+                        }
+
+                        // Output results
+                        dateTimeLbl.Text = $"{dateStr} at {timeStr}";
+                    }
+                    else // Not A-Z
+                        MessageBox.Show("Make sure last name starts with a letter!");
+                }
+                else
+                    MessageBox.Show("Enter a valid number of credit hours!");
+            }
+            else // Empty textbox
+                MessageBox.Show("Please enter last name!");
+        }
+    }
+}
---
+++ Program 4/Program 4/Program.cs
@@ -0,0 +1,201 @@
+﻿// Grading ID: J3408
+// Program 4
+// 12/03/2019
+// CIS 199-01
+// This program uses multiple methods and two classes for checking out, updating, and listing library books
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+public class LibraryBook
+{
+    // strings for book information 
+    public string title;
+    public string author;
+    public string publisher;
+    public int copyrightYear;
+    public string callNumber;
+    public bool checkoutStatus;
+
+    
+    public LibraryBook(string book_title, string book_author, string book_publisher, int book_copyright, string book_callnumber) 
+    {
+        // parameters for sending out from librarybook method
+        
+        title = book_title;
+        author = book_author;
+        publisher = book_publisher;
+        copyrightYear = book_copyright;
+        callNumber = book_callnumber;
+        checkoutStatus = false; // boolean to false 
+    }
+
+    // Pre-condition: none
+    // Post-condition: sets value to title
+    public string Title
+    {
+        get
+        {
+            return title;
+        }
+
+        set
+        {
+            title = value;
+        }
+    }
+
+    // Pre-condition: none
+    // Post-condition: none
+    public string Author
+    {
+        get
+        {
+            return author;
+        }
+
+        set
+        {
+            author = value;
+        }
+    }
+
+    // Pre-condition: none
+    // Post-condition: sets new value for changes in book
+    public string Publisher
+    {
+        get
+        {
+            return publisher;
+        }
+
+        set
+        {
+            publisher = value;
+        }
+    }
+
+    // Pre-condition: none
+    // Post-condition: none
+    public int CopyrightYear
+    {
+        get
+        {
+            return copyrightYear;
+        }
+
+        set
+        {
+            copyrightYear = value;
+        }
+    }
+
+    // Pre-condition: none
+    // Post-condition: sets new value for changes in book
+    public string CallNumber
+    {
+        get
+        {
+            return callNumber;
+        }
+
+        set
+        {
+            callNumber = value;
+        }
+    }
+
+    // Pre-condition: none
+    // Post-condition: sets method will set checkout status to true if the book has been already checked out
+    public void CheckOut()
+    {
+        checkoutStatus = !checkoutStatus;
+    }
+
+    // Pre-condition: none
+    // Post-condition: sets checkout status to true if the book has been returned
+    public void ReturnToShelf()
+    {
+        checkoutStatus = !checkoutStatus;
+    }
+
+    // Pre-condition: none
+    // Post-condition: returns true if the book is checked out and false when not returns true if the book is checked out and false when not 
+    public bool IsCheckedOut() 
+    {
+        return checkoutStatus;
+    }
+
+    // Pre-condition: none
+    // Post-condition: formats the strings for book details and overrides 
+    public override string ToString()
+    {
+        string result = "";
+
+        result += "Title: " + title + Environment.NewLine;
+        result += "Author: " + author + Environment.NewLine;
+        result += "Publisher: " + publisher + Environment.NewLine;
+        result += "Copyright Year: " + copyrightYear + Environment.NewLine;
+        result += "Call Number: " + callNumber + Environment.NewLine;
+        result += "Checkout Status: " + checkoutStatus + Environment.NewLine;
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // most popular books of 2019 hardcorded 
+        LibraryBook book1 = new LibraryBook("The Testaments", "Margaret Atwood", "Doubleday", 2019, "LC.2502.M3.A8 2019");
+        LibraryBook book2 = new LibraryBook("The Silent Patient", "Alex Michaelides", "Celadon Books", 2019, "JD.71.P453.2019");
+        LibraryBook book3 = new LibraryBook("City of Girls", "Elizabeth Gilbert", "Riverhead Books", 2019, "QL.342.N743.2019");
+        LibraryBook book4 = new LibraryBook("The Nickel Boys", "Colson Whitehead", "Doubleday", 2019, "N.42.G252 2019");
+        LibraryBook book5 = new LibraryBook("Three Women", "Lisa Taddeo", "Simon & Schuster", 2019, "GN.21.5.M4 2019");
+
+        // stores the books in an array
+        LibraryBook[] books = { book1, book2, book3, book4, book5 };
+
+        // ouputing books array to console
+        WriteLine("Books Currently in Library");
+        WriteLine();
+        printBooks(books);
+
+        // changes the book's information when checked out 
+        book1.Publisher = "AppleTree Publisher";
+        book2.CheckOut();
+        book3.CallNumber = "JO.204.A4 2019";
+        book4.CheckOut();
+        book5.Publisher = "Thomas E. Broderick";
+
+        // outputs the above method's changes to console
+        WriteLine("Updating Library Books");
+        WriteLine();
+        printBooks(books);
+
+        // calls two books to the checkout method when checked out
+        book1.CheckOut();
+        book2.CheckOut();
+
+        // Final list of library books and shows books array
+        WriteLine("Final List of Library Books");
+        WriteLine();
+        printBooks(books);
+    }
+
+    // Pre-condition: none
+    // Post-condition: makes changes in books array
+    public static void printBooks(LibraryBook[] books)
+    {
+        for (int index = 0; index < books.Length; ++index)
+        {
+            Console.WriteLine(books[index]);
+        }
+    }
+}
